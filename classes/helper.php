9b21296607cf11f1c9ea11c7be89add5d0249516
lushonline <56685070+lushonline@users.noreply.github.com>
2020-08-13 14:51:26 +0100

    Update helper.php
    Fixes #2 - Fixes #11 by using core get_default() function for category

--- classes/helper.php
+++ classes/helper.php
@@ -85,37 +85,36 @@ class tool_uploadexternalcontent_helper {
      * @return int category ID.
      */
     public static function resolve_category_by_id_or_idnumber($id) {
-        global $DB;
+        global $CFG, $DB;
 
+        // Handle null id by selecting the first non zero category id.
         if (is_null($id)) {
-            try {
-                $gettop1categories = $DB->get_records('course_categories', null, 'id asc', '*', 0, 1);
-                if (count($gettop1categories) != 0) {
-                    $firstcategory = array_pop($gettop1categories);
-                    $id = $firstcategory->id;
-                }
+            if (method_exists('\core_course_category', 'create')) {
+                $id = core_course_category::get_default()->id;
                 return $id;
-            } catch (Exception $e) {
-                return null;
+            } else {
+                require_once($CFG->libdir . '/coursecatlib.php');
+                $id = coursecat::get_default()->id;
+                return $id;
             }
+            return null;
         }
 
+        // Handle numeric id by confirming it exists.
         $params = array('id' => $id);
         if (is_numeric($id)) {
             if ($DB->record_exists('course_categories', $params)) {
                 return $id;
-            } else {
-                return null;
             }
-        } else {
-            $params = array('idnumber' => $id);
-            try {
-                $id = $DB->get_field_select('course_categories', 'id', 'idnumber = :idnumber', $params, MUST_EXIST);
-                return $id;
-            } catch (Exception $e) {
-                return null;
-            }
+            return null;
         }
+
+        // Handle any other id format by treating as a string idnumber value.
+        $params = array('idnumber' => $id);
+        if ($id = $DB->get_field_select('course_categories', 'id', 'idnumber = :idnumber', $params, MUST_EXIST)) {
+            return $id;
+        }
+        return null;
     }
 
     /**
